8e2eb827f8f79892a5c233efbd78de8db4f06923
karanjalight <karanjalight@gmail.com>
2025-03-07 00:14:21 +0300

    adding the PWA workbox

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,50 +56,50 @@ export default defineNuxtConfig({
     // Image optimization options
   },
   devtools: { enabled: false },
-  // pwa: {
-  //   manifest: {
-  //     name: "Smart One",
-  //     short_name: "Smart One",
-  //     description: "Water Bill Management System",
-  //     theme_color: "#ffffff",
-  //     icons: [
-  //       {
-  //         src: "/logo.png",
-  //         sizes: "192x192",
-  //         type: "image/png",
-  //       },
-  //       {
-  //         src: "/logo.png",
-  //         sizes: "512x512",
-  //         type: "image/png",
-  //       },
-  //     ],
-  //   },
-  //   registerType: "autoUpdate",
-  //   workbox: {
-  //     globDirectory:
-  //       process.env.NODE_ENV === "production"
-  //         ? ".output/public" // Production
-  //         : ".nuxt/dist", // Development
-  //     globPatterns: ["**/*.{js,css,html,png,svg,ico,json,woff2}"],
-  //     runtimeCaching: [
-  //       {
-  //         urlPattern: ({ url }) => url.origin === self.location.origin,
-  //         handler: "StaleWhileRevalidate",
-  //         options: {
-  //           cacheName: "local-assets-cache",
-  //           expiration: {
-  //             maxEntries: 50,
-  //             maxAgeSeconds: 86400, // Cache for 1 day
-  //           },
-  //         },
-  //       },
-  //     ],
-  //   },
-  //   devOptions: {
-  //     enabled: true,
-  //     type: "module",
-  //   },
-  // },
+  pwa: {
+    manifest: {
+      name: "Smart One",
+      short_name: "Smart One",
+      description: "Water Bill Management System",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "/logo.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/logo.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    registerType: "autoUpdate",
+    workbox: {
+      globDirectory:
+        process.env.NODE_ENV === "production"
+          ? ".output/public" // Production
+          : ".nuxt/dist", // Development
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,json,woff2}"],
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.origin === self.location.origin,
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "local-assets-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 86400, // Cache for 1 day
+            },
+          },
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
   compatibilityDate: "2025-02-18",
 });
